e089a1848dc34cdea4f9607d74eafc6232b6480a
PratyakshaChauhaan <75519291+PratyakshaChauhaan@users.noreply.github.com>
2022-04-08 16:03:28 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+prediction = "";
+
+Webcam.set(
+    {
+        width:350,
+        hieght:300,
+        image_format:'png',
+        png_quality:90
+    }
+);
+
+camera=document.getElementById("camera");
+
+Webcam.attach('#camera');
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri){
+    document.getElementById("result").innerHTML="<img id='captured_img' src='"+data_uri+"'/> "
+    });
+}
